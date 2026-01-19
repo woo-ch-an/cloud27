cb3b6365ef18277a7ca5f06ed43ea41cd242cfb5
woo-ch-an <dncksdl112423@gmail.com>
2026-01-20 08:57:01 +0900

    tnrwp tnwjd

--- java_exam/src/java_exam/GetPrimeNumber.java
+++ java_exam/src/java_exam/GetPrimeNumber.java
@@ -4,22 +4,26 @@ public class GetPrimeNumber {
 
 	public static void main(String[] args) {
 		boolean isprimeNumber = false;
+		
+		int icount = 0;
 
-		for (int i =2; i< 100; i++) 
+		for (int i =2; i< 100000; i++) 
 		{
 			isprimeNumber = true;
-			for (int j = 2; j< i/2; j++) 
+			for (int j = 2; j<= i/2; j++) 
 			{
 				if(i % j == 0) {
 					// 소수 아님
 					isprimeNumber = false;
 				}				
-			}	
+			}
 			
 			if(isprimeNumber == true) {
 				System.out.println("소수 발견 : " + i);	
+				icount++;
 			}
 		}
+		System.out.println("총 " + icount + "개 의 소수 발견");
 	}
 
 }
